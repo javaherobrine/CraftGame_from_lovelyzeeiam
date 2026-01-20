91ace2e23f53ac3c383bb46135853aa75233aa4b
javaherobrine <javaherobrine@qq.com>
2026-01-20 21:50:40 +0800

    Guess what! Java_Herobrine is still using AWT & Swing

--- src/test/java/xueli/awacoder/AboutQTDialog.java
+++ src/test/java/xueli/awacoder/AboutQTDialog.java
@@ -1,18 +1,6 @@
 package xueli.awacoder;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
-import javax.swing.UIManager;
-import javax.swing.border.EmptyBorder;
+import module java.desktop;
 
 public class AboutQTDialog extends JDialog {
 
@@ -37,7 +25,7 @@ public class AboutQTDialog extends JDialog {
 		panelBottom.setLayout(new FlowLayout(FlowLayout.RIGHT, 5, 5));
 
 		JButton btnOK = new JButton("OK");
-		btnOK.addActionListener(e -> this.setVisible(false));
+		btnOK.addActionListener(e -> this.dispose());
 		btnOK.setAlignmentX(Component.RIGHT_ALIGNMENT);
 		panelBottom.add(btnOK);
 
@@ -56,10 +44,10 @@ public class AboutQTDialog extends JDialog {
 		getContentPane().add(panelCenter, BorderLayout.CENTER);
 		panelCenter.setLayout(new BorderLayout(0, 0));
 
-		JLabel lblAboutContent = new JLabel(
-				"<html>\r\n<b>About Qt</b>\r\n<p></p>\r\n<p>This program uses Qt version 6.4.0.</p>\r\n<p></p>\r\n<p>Qt is a C++ toolkit for cross-platform application development.</p>\r\n<p></p>\r\n<p>Qt provides single-source portability across all major desktop operating systems. I is also available for embedded Linux and other embedded and mobile operating systems.</p>\r\n<p></p>\r\n<p>Qt is available under multiple licensing options designed to accommodate the needs of our various users.</p>\r\n<p></p>\r\n<p>Qt licensed under our commercial license agreement is appropriate for development of proprietary/commercial software where you do not want to share any source code with third parties or otherwise cannot comply with the terms of GNU (L)GPL.</p>\r\n<p></p>\r\n<p>Qt licensed under GNU (L)GPL is appropriate for the development of Qt applications provided you can comply with the terms and conditions of the respective licenses.</p>\r\n<p></p>\r\n<p>Please see <a>qt.io/licensing</a> for an overview of Qt licensing.</p>\r\n<p></p>\r\n<p>Copyright (C) 2022 The Qt Company Ltd and other contributors.</p>\r\n<p></p>\r\n<p>Qt and the Qt logo are trademarks of the Qt Company Ltd.</p>\r\n<p></p>\r\n<p>Qt is The Qt Company Ltd product developed as an open source project. See <a>qt.io</a> for more information.</p>\r\n</html>");
-		lblAboutContent.setVerticalAlignment(SwingConstants.TOP);
-		lblAboutContent.setHorizontalAlignment(SwingConstants.LEFT);
+		JEditorPane lblAboutContent = new JEditorPane(
+				"text/html","<html>\r\n<b>About Qt</b>\r\n<p></p>\r\n<p>This program uses Qt version 6.4.0.</p>\r\n<p></p>\r\n<p>Qt is a C++ toolkit for cross-platform application development.</p>\r\n<p></p>\r\n<p>Qt provides single-source portability across all major desktop operating systems. I is also available for embedded Linux and other embedded and mobile operating systems.</p>\r\n<p></p>\r\n<p>Qt is available under multiple licensing options designed to accommodate the needs of our various users.</p>\r\n<p></p>\r\n<p>Qt licensed under our commercial license agreement is appropriate for development of proprietary/commercial software where you do not want to share any source code with third parties or otherwise cannot comply with the terms of GNU (L)GPL.</p>\r\n<p></p>\r\n<p>Qt licensed under GNU (L)GPL is appropriate for the development of Qt applications provided you can comply with the terms and conditions of the respective licenses.</p>\r\n<p></p>\r\n<p>Please see <a>qt.io/licensing</a> for an overview of Qt licensing.</p>\r\n<p></p>\r\n<p>Copyright (C) 2022 The Qt Company Ltd and other contributors.</p>\r\n<p></p>\r\n<p>Qt and the Qt logo are trademarks of the Qt Company Ltd.</p>\r\n<p></p>\r\n<p>Qt is The Qt Company Ltd product developed as an open source project. See <a>qt.io</a> for more information.</p>\r\n</html>");
+//		lblAboutContent.setVerticalAlignment(SwingConstants.TOP);
+//		lblAboutContent.setHorizontalAlignment(SwingConstants.LEFT);
 		panelCenter.add(lblAboutContent);
 
 		this.pack();
